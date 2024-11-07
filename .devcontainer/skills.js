8e1463710bd7b572509ee7733a2b40d9d9586f92
John Kelsey Irungu <167783718+john-kelsey@users.noreply.github.com>
2024-11-07 15:29:31 +0300

    Create skills.js

---
+++ .devcontainer/skills.js
@@ -0,0 +1,4 @@
+function calculateNumbers(var1, var2)               // Function to calculate numbers
+{
+    return var1 + var2;                               // Return the sum of the two numbers
+}
